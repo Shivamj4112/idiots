842556f658c0b229f5dcbbb99a4abdb4eb017339
rnwankit <rnwprogrammingankit@gmail.com>
2022-11-22 18:43:49 +0530

    commit

--- app/src/main/java/com/example/a8idiots/MainActivity.java
+++ app/src/main/java/com/example/a8idiots/MainActivity.java
@@ -5,43 +5,78 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText weight,height;
-    Button enter;
+    CheckBox read, dance, sleep;
     TextView txt;
-
+    String box = "";
+    Button next2;
+    RadioGroup gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        weight = findViewById(R.id.eweight);
-        height = findViewById(R.id.edtheight);
-        enter = findViewById(R.id.btnenter);
-        txt = findViewById(R.id.txtanswer);
+        read = findViewById(R.id.read);
+        dance = findViewById(R.id.dance);
+        sleep = findViewById(R.id.sleep);
+        txt = findViewById(R.id.text);
+
+        next2 = findViewById(R.id.next);
+        gender = findViewById(R.id.gender);
 
 
-        enter.setOnClickListener(new View.OnClickListener() {
+        next2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
 
-                float w = Float.parseFloat(weight.getText().toString());
-                float h = Float.parseFloat(height.getText().toString());
+                //  Check Box
+
+                box = "";
+
+                if (read.isChecked()) {
+
+                    box += "reading";
+
+                }
+                if (dance.isChecked()) {
+
+                    box += "dance";
 
 
-                float ans = (w / ((h)*(h))) * 10000;
 
-                txt.setText(""+ans);
+                }
+                if (sleep.isChecked()) {
+
+                    box += "sleep";
+
+
+
+
+                }
+
+
+                Toast.makeText(MainActivity.this, ""+box, Toast.LENGTH_SHORT).show();
+
+
+                //  Radio Button
+                int Gender = gender.getCheckedRadioButtonId();
+
+                RadioButton button = findViewById(Gender);
+
+                Toast.makeText(MainActivity.this, ""+button.getText().toString(), Toast.LENGTH_LONG).show();
 
             }
         });
-
 
 
     }
